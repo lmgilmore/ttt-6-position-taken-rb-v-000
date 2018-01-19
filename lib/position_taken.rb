bb03b5bc5a10624ff2fb5a588f70914619d86f5f
Lorraine M. Gilmore <gilmore.lorraine.m@gmail.com>
2018-01-19 16:40:30 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,16 @@
-# code your #position_taken? method here!
+def position_taken?(board, index=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+  
+  value = board[index]
+ 
+  if value == " " || value == "" || value == nil
+    return false
+  end
+  
+  if value == "X" || value == "O"
+    return true
+  end
+  
+  
+end
+
+
